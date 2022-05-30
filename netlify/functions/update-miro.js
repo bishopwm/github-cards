b584297f565059090fecf800682fd3b86345dd9c
Addison <addison@miro.com>
2022-05-30 15:02:19 +0200

    Update and log incoming information

--- netlify/functions/update-miro.js
+++ netlify/functions/update-miro.js
@@ -1,4 +1,19 @@
+import { createClient } from "@supabase/supabase-js";
+const supabase = createClient(
+  process.env.VITE_DATABASE_URL,
+  process.env.VITE_DATABASE_PUBLIC_KEY
+);
+
 exports.handler = async function (event, context) {
+  console.log("Event", event);
+  console.log("Context", context);
+
+  console.log("Body", event.body);
+
+  const { data, error } = await supabase
+    .from("card-mappings")
+    .select("gitHubIssueId", "1");
+
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello World" }),
