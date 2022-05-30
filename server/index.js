eeb40b095b6245512a38e67422f4a67ceec50c2d
Addison <addison@miro.com>
2022-05-30 13:29:46 +0200

    typo

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+app.get("/", (req, res) => {
+  console.log("Hello world");
+});
+
 app.post("/update-miro", (req, res) => {
   console.log(req.body);
 });
