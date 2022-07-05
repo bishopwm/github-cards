1c3118d4097d9ef54e696e5a014de6f42f295d00
Addison <addison@miro.com>
2022-07-05 12:21:47 +0200

    parse

--- netlify/functions/authorize.js
+++ netlify/functions/authorize.js
@@ -24,6 +24,8 @@ exports.handler = async function (event, context, callback) {
     const redirectUrl = `https://miro.com/app-install-completed/?client_id=${clientId}&team_id=${teamId}`;
     const url = `https://api.miro.com/v1/oauth/token?grant_type=authorization_code&client_id=${clientId}&client_secret=${process.env.MIRO_CLIENT_SECRET}&code=${code}&redirect_uri=${process.env.MIRO_REDIRECT_URL}`;
 
+    console.log("URL constructed", url);
+
     getToken(url);
 
     return {
